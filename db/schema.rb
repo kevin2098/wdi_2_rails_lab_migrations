b2ca497358d3c16f34e34ebc93a3a05ed3d15aea
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-22 17:15:34 -0500

    Added color migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122221221) do
+ActiveRecord::Schema.define(version: 20150122221431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150122221221) do
     t.date    "updated_at"
     t.text    "city"
     t.text    "state"
+    t.text    "color"
   end
 
 end
