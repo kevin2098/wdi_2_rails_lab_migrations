052b66553b22f3834eb42e05046d20ba3600e2ee
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-23 08:17:51 -0500

    Finished migrations for locations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122221431) do
+ActiveRecord::Schema.define(version: 20150123131645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20150122221431) do
     t.text    "city"
     t.text    "state"
     t.text    "color"
+    t.text    "flavor"
+    t.decimal "price"
   end
 
 end
